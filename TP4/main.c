8d90c2f8fa6e62e1bc4b6cf210f21f640b3011de
Pedro Molina Prozzi <molinaprozzi.pedro@gmail.com>
2021-08-08 15:18:22 -0300

    todo joya

--- TP4/main.c
+++ TP4/main.c
@@ -69,17 +69,55 @@ void printTempTask(){
 	
 }
 
+void printTempTask(){
+	//actualizo LCD
+	char formato[13] ;
+	temperature = (ADCGetRead()*0.488*10);//redondear para rriba?
+	uint8_t parteEntera = temperature/10;
+	uint8_t parteFraccionaria = temperature % 10;
+	snprintf(formato,sizeof(formato),"TEMP: %d,%d C",parteEntera,parteFraccionaria);
+	LCDclr();
+	LCDGotoXY(0,0);
+	if (parteEntera < 10){
+		LCDstring(formato,sizeof(formato)-2);
+		
+	}else LCDstring(formato,sizeof(formato)-1);
+	
+	TIMER2ResetPrintFlag();
+	
+	
+}
+
 void checkTempTask(){
 	//check temperatura y ejecuto actuadores
-	temperature = round((ADCGetRead()*0.488));
-	if (temperature > 24){
-		PORTB |=(1<<PB0); 
-	}else if (temperature < 17)
+	temperature = round((ADCGetRead()*0.488*10));
+	if (temperature > 240){
+		PORTB |=(1<<PB0);
+	}else if (temperature < 170)
 	{
 		PORTB |=(1<<PB1);
-	}else 
+	}else
 	{
 		PORTB = 0x00;
+	}
+	TIMER2ResetTempFlag();
+}
+
+funcionControlTemperatura
+{
+	voltaje = ADC_getVoltaje() //me guardo el voltaje de lectura del ADC
+	//obtengo la temperatura usando la funcion obtenida anteriormente pero primero multiplico por 10
+	// ya que estoy usando numeros enteros y queremos tener un decimal de precisión.
+	temperatura = voltaje * 0.488 * 10
+	
+	if (temperature > (24 grados)*10){
+		//ponemos el bit de PB0 en 1
+	}else if (temperature < (17 grados)*10)
+	{
+		//ponemos el bit de PB1 en 1
+	}else
+	{
+		//ponemos todo PORTB en 0
 	}
 	
 }
