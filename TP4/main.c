65519464a3848cc5c7b900af3dd773db3a214c3a
Pedro Molina Prozzi <molinaprozzi.pedro@gmail.com>
2021-08-08 20:35:48 -0300

    fin

--- TP4/main.c
+++ TP4/main.c
@@ -15,6 +15,7 @@
 
 
 
+
 #include "adc.h"
 #include "lcd.h"
 #include "timer2.h"
@@ -56,11 +57,12 @@ int main(void)
 /**********************************************************
 * Propósito de la función: Imprimir temperatura en el lcd
 ***********************************************************/
+
 void printTempTask(){
 	char formato[13] ;
 	temperature =round (ADCGetRead()*0.488*10);  //obtengo la temperatura usando el geter de read pero primero multiplico por 10											
-											// ya que estoy usando numeros enteros y queremos tener un decimal de precisión.
-	uint8_t parteEntera = temperature/10;	// separo en parte entera y parte decimal para la impresion
+												// ya que estoy usando numeros enteros y queremos tener un decimal de precisión.
+	uint8_t parteEntera = temperature/10;		// separo en parte entera y parte decimal para la impresion
 	uint8_t parteFraccionaria = temperature % 10;
 	snprintf(formato,sizeof(formato),"TEMP: %d,%d C",parteEntera,parteFraccionaria);
 	LCDclr();
@@ -76,6 +78,7 @@ void printTempTask(){
 * Propósito de la función: chequear la temperatura y activar la
 * estufa o ventilador
 ***********************************************************/
+
 void checkTempTask(){
 	temperature = round(ADCGetRead()*0.488*10);//redondeo al entero mas cercano
 	if (temperature > 240){
@@ -88,6 +91,71 @@ void checkTempTask(){
 		PORTB = 0x00;
 	}
 }
+
+//CON FLOAT
+/*
+static volatile float temperature;
+
+
+int main(void)
+{
+	DIDR0= 0x01;//ADC0 como analog input
+	DDRB = 0x03;
+
+	
+	ADCInit();
+	LCDinit();
+	LCDclr();
+	LCDGotoXY(0,0);
+	TIMER2Init();
+	ADCStart();
+	sei();
+	
+	while (1)
+	{
+		if (TIMER2GetPrintFlag() == 2){ //flag que me define cuantas interrupciones hubo.
+			printTempTask();			//verifico que se hayan dado dos interrupciones para actualizar el LCD cada 0.5 segundos.
+			TIMER2ResetPrintFlag();		//reseteo el flag
+		}
+		if(TIMER2GetTempFlag() == 1){	//flag que me define si debo de ejecutar la lógica de control de temperatura.
+			checkTempTask();			//ejecuto el control de la temperatura.
+			TIMER2ResetTempFlag();		//reseteo el flag
+		}
+	}
+}
+
+/**********************************************************
+* Propósito de la función: Imprimir temperatura en el lcd
+***********************************************************/
+/*
+void printTempTask(){
+	char formato[13] ;
+	temperature =ADCGetRead()*0.488;  //obtengo la temperatura usando el geter
+	snprintf(formato,sizeof(formato),"TEMP: %.1f C",temperature);
+	
+	LCDclr();
+	LCDGotoXY(0,0);					//solo se impirme un digito de parte entera
+	LCDstring(formato,sizeof(formato)-1);
+}
+
+/**********************************************************
+* Propósito de la función: chequear la temperatura y activar la
+* estufa o ventilador
+***********************************************************/
+/*
+void checkTempTask(){
+	temperature = ADCGetRead()*0.488;//redondeo al entero mas cercano
+	if (temperature > 24){
+		PORTB |=(1<<PB0);
+	}else if (temperature < 17)
+	{
+		PORTB |=(1<<PB1);
+	}else
+	{
+		PORTB = 0x00;
+	}
+}
+*/
 
 
 
